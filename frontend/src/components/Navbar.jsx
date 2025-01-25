f373c1a3ad90922113fb9a7e1e772e288f07900d
navneeth99 <donthinavneethreddy18@gmail.com>
2025-01-26 00:23:09 +0530

    style changes in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,20 +7,20 @@ const Navbar = () => {
         <h1 className="text-xl font-bold">Health Tracker</h1>
         <ul className="flex space-x-6">
           <li>
-            <Link to="" className="hover:text-red-600">
+            <Link to="" className="hover:text-red-600 focus:text-red-600">
               Login/Register
             </Link>
           </li>
           <li>
-            <Link to="symptoms" className="hover:text-red-600">
+            <Link to="symptoms" className="hover:text-red-600 focus:text-red-600 ">
               Symptoms Form
             </Link>
           </li>
           <li>
-             <Link to="chat-bot" className="hover:text-red-600" >Chat Bot</Link>
+             <Link to="chat-bot" className="hover:text-red-600 focus:text-red-600" >Chat Bot</Link>
           </li>
           <li>
-            <Link to="user-profile" className="hover:text-red-600">Profile</Link>
+            <Link to="user-profile" className="hover:text-red-600 focus:text-red-600">Profile</Link>
           </li>
           
         </ul>
